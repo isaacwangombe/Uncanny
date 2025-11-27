51e825f7bef261e8ae6bc8a3d3ff3c4eef6efed3
isaac <isaac.wangombe@ukiyosoftware.com>
2025-11-27 06:24:12 +0300

    2 auto deploy

--- backend/uncannyvalleycomics/settings.py
+++ backend/uncannyvalleycomics/settings.py
@@ -228,6 +228,8 @@ USE_TZ = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
