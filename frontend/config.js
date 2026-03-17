6e1d4d1d4557b473ff9de8d7d0a4112e805d75d9
isaac <isaac.wangombe@ukiyosoftware.com>
2026-03-17 17:10:09 +0300

    changing /api

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
-window.API_BASE_URL_SHORT = "http://167.99.192.20:8080";
-// window.API_BASE_URL_SHORT = "/api";
+// window.API_BASE_URL_SHORT = "http://167.99.192.20:8080";
+window.API_BASE_URL_SHORT = "https://uncannyvalleycomics.com";
 // window.API_BASE_URL_SHORT = "http://134.209.18.2:8080";
 // window.API_BASE_URL_SHORT = "http://127.0.0.1:8000";
